0d28447336495c11ccd984011ea7fe8bd931ef14
Liam Dao <53534482+LiamDao@users.noreply.github.com>
2021-10-11 00:18:54 -0400

    example play animation provided by Tom Bliss

--- play_animation.py
+++ play_animation.py
@@ -20,22 +20,22 @@ tracking2020 = pd.read_csv('tracking2020.csv')
 
 #%% Merge scouting, game, player, and play data onto tracking data
 # 2018 season
-tracking2018 = tracking2018.merge(pff, on = ['gameId', 'playId'])
-tracking2018 = tracking2018.merge(games, on = 'gameId')
-tracking2018 = tracking2018.merge(players, on = 'nflId')
-tracking2018 = tracking2018.merge(plays, on = ['gameId', 'playId'])
+#tracking2018 = tracking2018.merge(pff, on = ['gameId', 'playId'])
+#tracking2018 = tracking2018.merge(games, on = 'gameId')
+#tracking2018 = tracking2018.merge(players, on = 'nflId')
+#tracking2018 = tracking2018.merge(plays, on = ['gameId', 'playId'])
 
 # View missing data
-missing = tracking2018.isnull().sum().sort_values(ascending = False)
+#missing = tracking2018.isnull().sum().sort_values(ascending = False)
 
 # 2019 season
-tracking2019 = tracking2019.merge(pff, on = ['gameId', 'playId'])
-tracking2019 = tracking2019.merge(games, on = 'gameId')
-tracking2019 = tracking2019.merge(players, on = 'nflId')
-tracking2019 = tracking2019.merge(plays, on = ['gameId', 'playId'])
+#tracking2019 = tracking2019.merge(pff, on = ['gameId', 'playId'])
+#tracking2019 = tracking2019.merge(games, on = 'gameId')
+#tracking2019 = tracking2019.merge(players, on = 'nflId')
+#tracking2019 = tracking2019.merge(plays, on = ['gameId', 'playId'])
 
 # View missing data
-missing = tracking2019.isnull().sum().sort_values(ascending = False)
+#missing = tracking2019.isnull().sum().sort_values(ascending = False)
 
 # 2020 season
 tracking2020 = tracking2020.merge(pff, on = ['gameId', 'playId'])
@@ -47,20 +47,17 @@ tracking2020 = tracking2020.merge(plays, on = ['gameId', 'playId'])
 missing = tracking2020.isnull().sum().sort_values(ascending = False)
 
 #%% Separate data into kicks and punts
-kicks2018 = tracking2018[tracking2018['kickType'].isin(['D', 'F', 'K', 'O', 
-                                                       'P', 'Q', 'S', 'B'])]
-kicks2019 = tracking2019[tracking2019['kickType'].isin(['D', 'F', 'K', 'O', 
-                                                       'P', 'Q', 'S', 'B'])]
-kicks2020 = tracking2020[tracking2020['kickType'].isin(['D', 'F', 'K', 'O', 
-                                                       'P', 'Q', 'S', 'B'])]
+       
+#punts2018 = tracking2018[tracking2018['specialTeamsPlayType'] == 'Punt']
+#punts2019 = tracking2019[tracking2019['specialTeamsPlayType'] == 'Punt']
+punts2020 = tracking2020[tracking2020['specialTeamsPlayType'] == 'Punt']
 
-punts2018 = tracking2018[tracking2018['kickType'].isin(['N', 'R', 'A'])]
-punts2019 = tracking2019[tracking2019['kickType'].isin(['N', 'R', 'A'])]
-punts2020 = tracking2020[tracking2020['kickType'].isin(['N', 'R', 'A'])]
-
-# Subset on Kickoff Team Recovery
-korecov2018 = kicks2018[kicks2018['specialTeamsResult'] == 'Kickoff Team Recovery']
 
 #%% Subset on Panthers and Patriots plays where they are the punting team (2020)
 panthers2020 = punts2020[ punts2020[ 'possessionTeam' ] == 'CAR' ]
 patriots2020 = punts2020[ punts2020[ 'possessionTeam' ] == 'NE']
+
+panthers2020.to_csv('Panthers2020.csv')
+patriots2020.to_csv('Patriots2020.csv')
+
+#%% 
